30cfd146b01e4aef8e013f00c004b82dbbe733e0
YAHYA <yahya.benhajomor@gmail.col>
2025-10-13 14:45:05 +0100

    ref

--- src/app/core/models/models.ts
+++ src/app/core/models/models.ts
@@ -222,7 +222,7 @@ export interface Candidate {
   birthDate: null;
   age: null;
   role: string;
-  localtion: string | null;
+  location: string | null;
   emailVerifiedAt: Date | null;
   profileTitle: string | null;
   aiId: null;
