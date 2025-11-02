66260495e98589bdd6333ce1eafc4ebe6d035ed6
YAHYA <yahya.benhajomor@gmail.col>
2025-11-02 22:54:13 +0100

    ref

--- src/app/features-modules/pages/recrutement-entrprise/recrutment-entrprise-routing.module.ts
+++ src/app/features-modules/pages/recrutement-entrprise/recrutment-entrprise-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { RecrutmentEntrprise } from './recrutmentEntrprise';
- 
+import { RecrutmentEntrprise } from './RecrutmentEntrprise';
+  
 const routes: Routes = [{ path: '', component: RecrutmentEntrprise }];
 
 @NgModule({
