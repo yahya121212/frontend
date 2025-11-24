28562aaf23455780b84d65c7794ce104425bb762
YAHYA <yahya.benhajomor@gmail.col>
2025-11-24 14:52:52 +0100

    ref

--- src/app/features-modules/freelancer/project/project-list/project-list-routing.module.ts
+++ src/app/features-modules/freelancer/project/project-list/project-list-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProjectListComponent } from './project-list.component';
 
-const routes: Routes = [{ path: '',title:"Découvrez nos offres d’emploi – Rejoignez Société Intérim Online Protec", component: ProjectListComponent }];
+const routes: Routes = [{ path: '',title:"Missions d’intérim disponibles | InterimOnline", component: ProjectListComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/features-modules/freelancer/project/projects/projects-routing.module.ts
+++ src/app/features-modules/freelancer/project/projects/projects-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProjectsComponent } from './projects.component';
 
-const routes: Routes = [{ path: '',title:"Découvrez nos offres d’emploi – Rejoignez Société Intérim Online Protec",  component: ProjectsComponent }];
+const routes: Routes = [{ path: '',title:"Missions d’intérim disponibles | InterimOnline",  component: ProjectsComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
